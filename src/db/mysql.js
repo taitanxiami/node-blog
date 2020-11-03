01c42be2f3928de68100b314a9043471dd9b6779
taitanxiami <taitanxiami@gmail.com>
2020-11-03 23:52:21 +0800

    conf mysql

---
+++ src/db/mysql.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql')
+
+const {MYSQL_CONF} = require('../conf/db')
+
+
+const con = mysql.createConnection(MYSQL_CONF)
+
+con.connect()
+const exec= (sql) => {
+    return new Promise((resolve,reject) => {
+
+        con.query(sql, (err,result)=> {
+            if(err) {
+                reject(err)
+            }
+            resolve(result)
+        })
+    })
+}
+
+
+
+module.exports = {
+    exec
+}
